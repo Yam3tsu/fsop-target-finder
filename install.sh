f229003957ef02eeee5b850992547208733db52a
Yam3tsu <davideno58@gmail.com>
2025-09-18 00:30:40 +0200

    Modified install.sh such that it will throw some errors  if launched from other directories than the installation one.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 PWD=$(pwd)
 
 make
-sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' ./main.py
-sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' ./gdb_daemon.py
+sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' $PWD/main.py
+sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' $PWD/gdb_daemon.py
 sudo ln -f -s $PWD/main.py /usr/bin/fsop-target-finder
