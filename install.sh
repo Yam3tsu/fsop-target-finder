fc9d4b91296a5ac86681335ab111de0da16a0da4
Yam3tsu <davideno58@gmail.com>
2025-09-21 14:45:52 +0200

    Changed tool name in /usr/bin

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ make -C $PWD/src --quiet
 sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' $PWD/src/modules/constants.py
 sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' $PWD/src/modules/gdb_find_vtable_offset.py
 sed -i -E 's|^(INSTALLATION_PATH = )".*"$|\1"'"$PWD"'"|' $PWD/src/modules/gdb_find_wfile_overflow.py
-sudo ln -f -s $PWD/src/find_vtable_offset.py /usr/bin/fsop-target-finder
+sudo ln -f -s $PWD/src/find_vtable_offset.py /usr/bin/fsop-find-vtable-offset
 sudo ln -f -s $PWD/src/find_wfile_overflow.py /usr/bin/fsop-wfile-overflow-finder
